38a257c79d291ea046d41be48d14382a865b88a2
P1F <rtsouza15@gmail.com>
2021-09-26 11:38:58 -0400

    added SharedResource.go

---
+++ SharedResource.go
@@ -0,0 +1,13 @@
+func main() {
+	Address, err := net.ResolveUDPAddr("udp", ":10001")
+	CheckError(err)
+	Connection, err := net.ListenUDP("udp", Address)
+	CheckError(err)
+	defer Connection.Close()
+	for {
+		//Loop infinito para receber mensagem e escrever todo
+		//conteúdo (processo que enviou, relógio recebido e texto)
+		//na tela
+		//FALTA FAZER
+	}
+}
