f9aea0347a5c516770c043d5a5d41b752cbc8b64
P1F <rtsouza15@gmail.com>
2021-09-26 19:55:04 -0400

    position terminal where u want

--- runProcesses.sh
+++ runProcesses.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gnome-terminal --title="SharedResource" -e 'sh -c "go run SharedResource.go"'
-gnome-terminal --title="Process 1" -e 'sh -c "go run Process.go 1 :10002 :10003 :10004 :10005"'
-gnome-terminal --title="Process 2" -e 'sh -c "go run Process.go 2 :10002 :10003 :10004 :10005"'
-gnome-terminal --title="Process 3" -e 'sh -c "go run Process.go 3 :10002 :10003 :10004 :10005"'
-gnome-terminal --title="Process 4" -e 'sh -c "go run Process.go 4 :10002 :10003 :10004 :10005"'
+gnome-terminal --geometry 800x500+0+0 --title="SharedResource" -e 'sh -c "go run SharedResource.go"'
+gnome-terminal --geometry 800x500+0+0 --title="Process 1" -e 'sh -c "go run Process.go 1 :10002 :10003 :10004 :10005"'
+gnome-terminal --geometry 800x500+1000+0 --title="Process 2" -e 'sh -c "go run Process.go 2 :10002 :10003 :10004 :10005"'
+gnome-terminal --geometry 800x500+0+1000 --title="Process 3" -e 'sh -c "go run Process.go 3 :10002 :10003 :10004 :10005"'
+gnome-terminal --geometry 800x500+1000+1000 --title="Process 4" -e 'sh -c "go run Process.go 4 :10002 :10003 :10004 :10005"'
