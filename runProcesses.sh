73dd67d67c7bfb8f72ca5097584dcbfaef3eb1a5
P1F <rtsouza15@gmail.com>
2021-09-26 17:42:38 -0400

    terminals with names

--- runProcesses.sh
+++ runProcesses.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gnome-terminal -e 'sh -c "go run SharedResource.go"'
-gnome-terminal -e 'sh -c "go run Process.go 1 :10002 :10003 :10004 :10005"'
-gnome-terminal -e 'sh -c "go run Process.go 2 :10002 :10003 :10004 :10005"'
-gnome-terminal -e 'sh -c "go run Process.go 3 :10002 :10003 :10004 :10005"'
-gnome-terminal -e 'sh -c "go run Process.go 4 :10002 :10003 :10004 :10005"'
+gnome-terminal --title="SharedResource" -e 'sh -c "go run SharedResource.go"'
+gnome-terminal --title="Process 1" -e 'sh -c "go run Process.go 1 :10002 :10003 :10004 :10005"'
+gnome-terminal --title="Process 2" -e 'sh -c "go run Process.go 2 :10002 :10003 :10004 :10005"'
+gnome-terminal --title="Process 3" -e 'sh -c "go run Process.go 3 :10002 :10003 :10004 :10005"'
+gnome-terminal --title="Process 4" -e 'sh -c "go run Process.go 4 :10002 :10003 :10004 :10005"'
