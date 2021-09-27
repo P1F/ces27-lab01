9e1ca346db1f58f081b8c49b6d0fa74bab7d3195
P1F <rtsouza15@gmail.com>
2021-09-27 15:04:55 -0400

    changes

--- runProcesses.sh
+++ runProcesses.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gnome-terminal --geometry 800x500+0+0 --title="SharedResource" -e 'sh -c "go run resource/SharedResource.go"'
-gnome-terminal --geometry 800x500+0+0 --title="Process 1" -e 'sh -c "go run Process.go 1 :10002 :10003 :10004 :10005"'
-gnome-terminal --geometry 800x500+1000+0 --title="Process 2" -e 'sh -c "go run Process.go 2 :10002 :10003 :10004 :10005"'
-gnome-terminal --geometry 800x500+0+1000 --title="Process 3" -e 'sh -c "go run Process.go 3 :10002 :10003 :10004 :10005"'
-gnome-terminal --geometry 800x500+1000+1000 --title="Process 4" -e 'sh -c "go run Process.go 4 :10002 :10003 :10004 :10005"'
+gnome-terminal --geometry 900x500+0+0 --title="SharedResource" -e 'sh -c "go run resource/SharedResource.go"'
+gnome-terminal --geometry 900x500+0+0 --title="Process 1" -e 'sh -c "go run Process.go 1 :10002 :10003 :10004 :10005"'
+gnome-terminal --geometry 900x500+1200+0 --title="Process 2" -e 'sh -c "go run Process.go 2 :10002 :10003 :10004 :10005"'
+gnome-terminal --geometry 900x500+0+1000 --title="Process 3" -e 'sh -c "go run Process.go 3 :10002 :10003 :10004 :10005"'
+gnome-terminal --geometry 900x500+1200+1000 --title="Process 4" -e 'sh -c "go run Process.go 4 :10002 :10003 :10004 :10005"'
